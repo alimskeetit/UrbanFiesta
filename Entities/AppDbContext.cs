eba629c5cab18ee7631ddaa5c60a026806c33205
Ivan <afkanovich@yandex.ru>
2023-04-26 20:52:39 +0300

    Добавьте файлы проекта.

---
+++ Entities/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    internal class AppDbContext: IdentityDbContext<Citizen>
+    {
+        DbSet<Citizen> Citizens => Set<Citizen>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+    }
+}
---
+++ Entities/Models/Citizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Entities.Models
+{
+    internal class Citizen : IdentityUser 
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string? Patronymic { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Citizen>? Family { get; set; }
+    }
+}
